bedc626550bbca6b65cf8d712f6fd2a736a8aa99
PLINIOGRAU37 <58754680+PLINIOGRAU37@users.noreply.github.com>
2020-12-04 23:16:17 -0500

    se agregan primer archivo

---
+++ tipodemotor.js
@@ -0,0 +1,11 @@
+function mostrarmensaje1() {
+    var motor;
+     
+    motor = prompt ('Por favor intruduce el tipo de motor (solo valido valores 1, 2, 3, 4) : ');
+    if (motor =="") { alert ('no has intruducido ningun tipo de motor');
+}
+motor = Number (motor);
+switch (motor) {
+    
+}
+
